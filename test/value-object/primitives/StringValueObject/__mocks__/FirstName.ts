82895a8ce2c3a6c328fbd8bcced9b3d831f4d53a
Matishac <matias.fernandez.zamorano@gmail.com>
2025-11-15 11:07:32 -0300

    update FirstName.ts v2

--- test/value-object/primitives/StringValueObject/__mocks__/FirstName.ts
+++ test/value-object/primitives/StringValueObject/__mocks__/FirstName.ts
@@ -1,4 +1,5 @@
-import { StringValueObject } from '../../../../src';
+// In test/value-object/primitives/StringValueObject/__mocks__/FirstName.ts
+import { StringValueObject } from 'pkg-value-object/src'; // or correct relative import
 
 export default class FirstName extends StringValueObject {
   constructor(firstName: string) {
